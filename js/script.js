83c14a7639f61ef79cd0056b59391c66965311cc
chinedm <eduman4real@gmail.com>
2023-11-09 13:15:45 +0100

    Mr_Amaechi Work so far

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Declare any global variables here
+
+// Define any helper functions here
+
+// Define the main function that will run when the page loads
+function main() {
+    // Your code here
+}
+
+// Call the main function when the page loads
+window.addEventListener('load', main);
